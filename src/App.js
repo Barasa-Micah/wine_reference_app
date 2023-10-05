c6728bea6ab75678ff5478cb1c3068c6e7fcf3f2
Moringa School <chepfaith18@gmail.com>
2023-10-05 22:10:13 +0300

    Added routes and scripts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AboutPage from "./pages/AboutPage";
+import Home from "./pages/Home";
+import Footer from "./pages/Footer";
+import Navbar from "./pages/Navbar";
+import Wines from "./pages/Wines";
+import Login from './pages/Login';
+import { AuthProvider } from './pages/AuthContext'; // Import the AuthProvider
+import Reviews from './pages/Reviews';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wines" element={<Wines />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
