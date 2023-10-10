51adf020b5cc094ed7177921b8b533e0dc84bde8
kipkirui63 <enocksang8356@gmail.com>
2023-10-10 23:02:08 +0300

    database created

---
+++ database/app/__init__.py
@@ -0,0 +1,34 @@
+# app.py
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ref_app_user:MTHL05kkxBXYY5YrTE7sapsEOP0kIwPI@dpg-ckiqatb6fquc739dcos0-a.ohio-postgres.render.com/ref_app'
+
+
+
+db = SQLAlchemy(app)
+
+
+def create_app(): 
+    CORS(app)
+
+    CORS(app, origins="*")
+
+    # db.init_app(app)
+
+    
+    # add routes
+    from app import routes
+
+    return app
+
+
+
+
+
+
+
+
+
---
+++ database/seed.py
@@ -0,0 +1,45 @@
+# from app import create_app, db
+# from flask_migrate import Migrate
+# from app.models import User
+
+# import sys
+
+# app = create_app()
+
+# migrate = Migrate(app, db)
+
+
+
+# def make_shell_context():
+#     return dict(app=app, db=db, user=User)
+
+
+
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#     # Check if the "--debug" or "-d" option is provided
+#         if "--debug" in sys.argv or "-d" in sys.argv:
+#             app.debug = True  # Enable debug mode if the option is provided
+
+#     app.run()
+
+from app import create_app, db
+from flask_migrate import Migrate
+from app.models import User
+
+app = create_app()
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User
+    }
+
+if __name__ == '_main_':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
