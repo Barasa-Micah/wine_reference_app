8a9aaca3a7584a5be44dde5343ee459c3c781792
Barasa-Micah <barasamica@gmail.com>
2023-10-16 12:44:43 +0300

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import Review from "./component/Review";
-import ReviewList from "./component/ReviewList";
-import PaymentPrompt from "./component/PaymentPrompt";
 
 
-
-function onSelectOption(option){
-  console.log('Selected option:', option);
-}
 
 function App() {
-  return (
-    <div> 
-       <h1>Wine App</h1>
-      <Review/>
-      <ReviewList/>
-      <PaymentPrompt />
-    </div>
-  );
+ 
 }
 
 export default App;
